9c3563f345486f17f45fad2bed31447cf4714a1c
Uthmaan <samsodienuthmaan@gmail.com>
2023-08-12 11:54:17 +0200

    update

--- api/model/Users.js
+++ api/model/Users.js
@@ -1,1 +1,65 @@
-///...
+const db = require("../config");
+class Users {
+  fetchUsers(req, res) {
+    const query = `
+        SELECT userID, firstName, lastName, gender, userDOB, emailAdd, profileUrl
+        FROM Users;
+        `
+        db.query(query,
+            (err, results)=>{
+                if(err) throw err
+                res.json({
+                    status: res.statusCode,
+                    results
+                })
+            })
+  }
+  fetchUser(req, res) {
+    const query = `
+    SELECT userID, firstName, lastName, gender, userDOB, emailAdd, profileUrl
+    FROM Users
+    WHERE userID = ${req.params.id};
+    `
+    db.query(query,
+        (err, result) => {
+            if(err) throw err
+            res.json({
+                status: res.statusCode,
+                result
+            })
+        })
+  }
+  login(req, res) {
+  }
+  register(req, res) {
+  }
+  updateUser(req, res) {
+    const query = `
+    UPDATE Users
+    SET ?
+    WHERE userID = ?
+    `
+    db.query(query,
+         [req.body, req.params.id],
+         (err)=>{
+            if(err) throw err
+            res.json({
+                status: res.statusCode,
+                msg: "Update Complete"
+            })
+         })
+  }
+  deleteUser(req, res) {
+    const query = `
+    DELETE FROM Users
+    WHERE userID = ${req.params.id};
+    `
+    db.query(query, (err)=>{
+        if(err) throw err
+        res.json({
+            status: statusCode,
+            msg: "Removal Complete"
+        })
+    })
+  }
+}
